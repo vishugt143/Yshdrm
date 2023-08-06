e5a1b6afdd42424361741ff5b6f05f1a70b6cc63
Diligent_Coder <84326470+diligent-coder-MANISH@users.noreply.github.com>
2023-08-06 15:26:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Config(object):
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
-    GROUPS = os.environ.get('GROUPS', '').split(',')
+    GROUPS = os.environ.get('GROUPS', '-10097927192)').split(',')
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
