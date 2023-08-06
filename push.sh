74d3739ecd54b2e5008161f310aa19eaf479f9ab
Your <arshtwitterbot@gmail.com>
2023-08-06 15:17:30 +0530

    first

---
+++ push.sh
@@ -0,0 +1,7 @@
+echo "# maalmasala" >> README.md
+git init
+git add README.md
+git commit -m "first conmit"
+git branch -M master
+git remote add origin https://github.com/diligent-coder-MANISH/maalmasala.git
+git push -u origin master
